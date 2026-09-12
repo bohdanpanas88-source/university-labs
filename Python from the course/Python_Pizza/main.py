400c5a55bcad5978e0f19b8c00cddbdae2728138
bohdanpanas88-source <bohdanpanas88@gmail.com>
2026-09-12 18:20:50 +0300

    Refactor pizza delivery game logic and output

--- Python from the course/Python_Pizza/main.py
+++ Python from the course/Python_Pizza/main.py
@@ -1,28 +1,46 @@
-print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M or L: ")
-pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-extra_cheese = input("Do you want extra cheese? Y or N: ")
+print(r'''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
 
-final_price = 0
-
-if size == "S":
-    final_price += 15
-    if pepperoni == "Y":
-        final_price += 2
-elif size == "M":
-    final_price += 20
-    if pepperoni == "Y":
-        final_price += 3
-elif size == "L":
-    final_price += 25
-    if pepperoni == "Y":
-        final_price += 3
+where_go = input("Type \"left\" or \"right\"").lower()
+if where_go == "right" or where_go == "r":
+    print("Fall into a hole.\n\tGame Over")
+elif where_go == "left" or where_go == "l":
+    what_do = input("Type swim or wait: ").lower()
+    if what_do == "swim":
+        print("Attacked by trout.\n\tGame Over.")
+    elif what_do == "wait" or what_do == "w":
+        which_dor = input("Which door you chose? red, blue or yellow: ").lower()
+        if which_dor == "red" or which_dor == "r":
+            print("Burned by fire.\n\tGame Over.")
+        elif which_dor == "blue" or which_dor == "b":
+            print("Eaten by beasts.\n\tGame Over.")
+        elif which_dor == "yellow" or which_dor == "y":
+            print("\tYou Win!")
+        else:
+            print("\tGame Over")
+    else:
+        print("Attacked be trout.\n\tGame Over.")
 else:
-    print("Sorry. You type the wrong inputs!")
-
-
-
-if extra_cheese == "Y":
-    final_price += 1
-
-print(f"Your final bill is: ${final_price}.")
+    print("Game Over!")
